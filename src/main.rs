1efaea9f4b5b537c6b47d56b89f4c72b8b563cb5
Nicolas Frankel <nicolas@frankel.ch>
2021-05-08 19:08:12 +0200

    First working implementation to list Pods from the Kubernetes cluster in a specific namespace

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use k8s_openapi::api::core::v1::Pod;
+use kube::{api::ListParams, client::Client, Api};
 use log::info;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
-    info!("Hello, world!");
+    let client = Client::try_default().await.unwrap();
+    let api: Api<Pod> = Api::namespaced(client, "kube-system");
+    api.list(&ListParams::default())
+        .await
+        .unwrap()
+        .items
+        .iter()
+        .map(|pod| pod.metadata.name.as_ref().unwrap())
+        .for_each(|name| info!("{}", name));
 }
