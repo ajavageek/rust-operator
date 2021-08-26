08092329915535f915096193b1f2fa048f8bc808
Nicolas Frankel <nicolas@frankel.ch>
2021-08-26 15:06:03 +0200

    Refactor to more Rust-y code

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use k8s_openapi::api::core::v1::Pod;
+use k8s_openapi::Metadata;
+use kube::error::Error;
 use kube::{api::ListParams, client::Client, Api};
 use log::info;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     log4rs::init_file("log4rs.yml", Default::default()).unwrap();
     let client = Client::try_default().await.unwrap();
     let api: Api<Pod> = Api::namespaced(client, "kube-system");
-    api.list(&ListParams::default())
-        .await
-        .unwrap()
-        .items
-        .iter()
-        .map(|pod| pod.metadata.name.as_ref().unwrap())
-        .for_each(|name| info!("{}", name));
+    for p in api.list(&ListParams::default()).await? {
+        info!("Found Pod: {}", p.metadata().name.as_ref().unwrap());
+    }
+    Ok(())
 }
